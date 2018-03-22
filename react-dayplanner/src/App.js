91b222cec743729351c61c3f6a997d0742300c79
Sasimovich Andrey <ballack.tyta@gmail.com>
2018-03-22 14:47:17 +0300

    add todo form refactored

--- react-dayplanner/src/App.js
+++ react-dayplanner/src/App.js
@@ -9,12 +9,26 @@ import AddTodoForm from './components/AddTodoForm'
 import moment from 'moment'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      newTodo:{
+        startTime: "",
+        endTime: "",
+        description: ""
+      }
+    }
+  }
+
   componentDidMount(){
     this.props.getTodos();
   }
 
   renderTodoList(){
     const { todos } = this.props;
+
+    console.log("app rendered")
 
     return(Object.values(todos).map((todo) => 
           <Todo {...todo} key={todo.id}/>
@@ -28,18 +42,59 @@ class App extends Component {
       <div className="App">
         <UserView/>
         {this.renderTodoList()}
-        <AddTodoForm onSubmit={(newTodo) => this.onAddTodo(newTodo)}/>
+        <AddTodoForm 
+          {...this.state.newTodo}
+          onSubmit={() => this.onAddTodo()} 
+          onTodoChanged={(propName, value) => this.onNewTodoChanged(propName, value)}
+          // onStartTimeChanged={(startTime) => this.onStartTimeChanged(startTime)} 
+          // onEndTimeChanged={(endTime) => this.onEndTimeChanged(endTime)} 
+          // onDescriptionChanged={(description) => this.onDescriptionChanged(description)}
+        />
       </div>
     );
   }
 
-  onAddTodo(todoFromForm){
-    var newTodo = {
-        ...todoFromForm
-    }
+  onAddTodo(){
+    var newTodo = Object.assign({}, this.state.newTodo,{
+      startTime: moment(this.state.newTodo.startTime),
+      endTime: moment(this.state.newTodo.endTime)
+    })
+
+    this.setState({
+      newTodo:{
+        startTime: "",
+        endTime: "",
+        description: ""
+      }
+    })
 
     this.props.addTodo(newTodo);
   }
+
+  onNewTodoChanged(propName, value){
+    this.setState({
+      newTodo: Object.assign({}, this.state.newTodo, {[propName]: value})
+    })
+  }
+
+  // onStartTimeChanged(startTime){
+  //   this.setState({
+  //     newTodo: Object.assign({}, this.state.newTodo, {startTime: startTime})
+  //   })
+  // }
+
+  // onEndTimeChanged(endTime){
+  //   this.setState({
+  //     newTodo: Object.assign({}, this.state.newTodo, {endTime: endTime})
+  //   })
+  // }
+
+  // onDescriptionChanged(description){
+  //   this.setState({
+  //     newTodo: Object.assign({}, this.state.newTodo, {description: description})
+  //   })
+  // }
+  
 }
 
 function mapStateToProps (state) {
--- react-dayplanner/src/components/AddTodoForm.js
+++ react-dayplanner/src/components/AddTodoForm.js
@@ -2,34 +2,18 @@ import React, { Component } from 'react'
 import moment from 'moment'
 
 export default class AddTodoForm extends Component{
-    constructor(props){
-        super(props);
-
-        this.state = {
-            startDate: "",
-            endDate: "",
-            description: ""
-        }
-    }
-
-    onSubmit(){
-        var newTodo = {
-            startTime: moment(this.state.startDate),
-            endTime: moment(this.state.endDate),
-            description: this.state.description
-        }
-
-        this.props.onSubmit(newTodo);
-    }
-
     render(){
         return(
             <div>
-                Start date <input value={this.state.startDate} type="text"  onChange={(ev) => this.setState({startDate: ev.target.value})}/><br/>
-                End date <input value={this.state.endDate} type="text"  onChange={(ev) => this.setState({endDate: ev.target.value})}/><br/>
-                Description <input value={this.state.description} type="text"  onChange={(ev) => this.setState({description: ev.target.value})}/><br/>
-                <input type="button" onClick={ () => this.onSubmit() } value="add todo"/>
+                Start date <input value={this.props.startTime} type="text"  onChange={(ev) => this.onTodoChanged("startTime", ev.target.value)}/><br/>
+                End date <input value={this.props.endTime} type="text"  onChange={(ev) => this.onTodoChanged("endTime", ev.target.value)}/><br/>
+                Description <input value={this.props.description} type="text"  onChange={(ev) => this.onTodoChanged("description", ev.target.value)}/><br/>
+                <input type="button" onClick={ () => this.props.onSubmit() } value="add todo"/>
             </div>
         )
+    }
+
+    onTodoChanged(propName, value){
+        this.props.onTodoChanged(propName, value);
     }
 }
